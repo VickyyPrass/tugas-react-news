0344c2f5b911a534618ff33aee41d90666309d38
VickyyPrass <vickygalih31@gmail.com>
2023-04-29 23:12:56 +0700

    hapus style.scss

--- src/Komponen/index.jsx
+++ src/Komponen/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import SearchDataNews from "../DataNews/getDataSearch";
 import NavbarApp from "./Navbar";
-import "./style.scss";
 
 export default class ArtikelApp extends React.Component {
     render() {
